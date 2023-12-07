b5b86449740e42f1031db0b914a77938e90f473a
Ibroxim <ibrohimismoilov738@gmail.com>
2023-12-07 10:21:48 +0500

    init

---
+++ js/sliderAndNavbarMenu.js
@@ -0,0 +1,36 @@
+const slides = document.getElementsByClassName("slide");
+const dots = document.getElementsByClassName("dot");
+const closeNavbarMenuIcon = document.querySelector(".close__navbar__menu__icon");
+const navbarMenuIcon = document.querySelector(".navbar__menu__icon");
+const navbarTop = document.querySelector(".navbar__top");
+
+var currentIndex = 1;
+
+function displaySlide(n) {
+    currentIndex = n;
+
+    if (currentIndex > slides.length) {
+        currentIndex = 1;
+    }
+    if (currentIndex < 1) {
+        currentIndex = slides.length;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[currentIndex - 1].style.display = "block";
+    dots[currentIndex - 1].className = "dot active";
+}
+
+displaySlide(currentIndex);
+
+function changeSlide(n) {
+    currentIndex += n;
+    displaySlide(currentIndex);
+}
+
+function currentSlide(n) {
+    displaySlide(n);
+}

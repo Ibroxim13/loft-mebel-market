9f0d19437edc041d0026d7139a572fede7e676d1
Ibroxim <ibrohimismoilov738@gmail.com>
2023-12-07 14:38:49 +0500

    added func to navbar menu

--- js/sliderAndNavbarMenu.js
+++ js/sliderAndNavbarMenu.js
@@ -4,6 +4,14 @@ const closeNavbarMenuIcon = document.querySelector(".close__navbar__menu__icon")
 const navbarMenuIcon = document.querySelector(".navbar__menu__icon");
 const navbarTop = document.querySelector(".navbar__top");
 
+navbarMenuIcon.addEventListener("click", () => {
+    navbarTop.style.left = 0;
+})
+
+closeNavbarMenuIcon.addEventListener("click", () => {
+    navbarTop.style.left = "-100%";
+})
+
 var currentIndex = 1;
 
 function displaySlide(n) {
